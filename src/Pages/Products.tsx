c6edc967518178515502ed7309bbe50eab177047
3ugenio <eugenio.gferrerd@gmail.com>
2025-09-06 12:16:28 -0400

    fix

--- src/Pages/Products.tsx
+++ src/Pages/Products.tsx
@@ -35,120 +35,96 @@ const Products = () => {
 
   // Evento para agregar producto
  
-  // const handleAdd = async (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //    if (form.price && isNaN(Number(form.price))) {
-  //   alert("El precio debe ser un número válido");
-  //   setForm((prev) => ({ ...prev, price: "" }));
-  // }
-  // if (form.price && Number(form.price) < 0) {
-  //   alert("El precio no puede ser negativo");
-  //   setForm((prev) => ({ ...prev, price: "" }));
-  // }
-  // if (form.stock && isNaN(Number(form.stock))) {
-  //   alert("El stock debe ser un número válido");
-  //   setForm((prev) => ({ ...prev, stock: "" }));
-  // }
-  // if (form.stock && Number(form.stock) < 0) {
-  //   alert("El stock no puede ser negativo");
-  //   setForm((prev) => ({ ...prev, stock: "" }));
-  // }
+  const handleAdd = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+     if (form.price && isNaN(Number(form.price))) {
+    alert("El precio debe ser un número válido");
+    setForm((prev) => ({ ...prev, price: "" }));
+  }
+  if (form.price && Number(form.price) < 0) {
+    alert("El precio no puede ser negativo");
+    setForm((prev) => ({ ...prev, price: "" }));
+  }
+  if (form.stock && isNaN(Number(form.stock))) {
+    alert("El stock debe ser un número válido");
+    setForm((prev) => ({ ...prev, stock: "" }));
+  }
+  if (form.stock && Number(form.stock) < 0) {
+    alert("El stock no puede ser negativo");
+    setForm((prev) => ({ ...prev, stock: "" }));
+  }
 
-  // if (form.category_id && isNaN(Number(form.category_id))) {
-  //   alert("El ID de categoría debe ser un número válido");
-  //   setForm((prev) => ({ ...prev, category_id: "" }));
-  // }
-  // if (form.id && isNaN(Number(form.id))) {
-  //   alert("El ID del producto debe ser un número válido");
-  //   setForm((prev) => ({ ...prev, id: "" }));
-  // }
-  // if (form.id && Number(form.id) < 1) {
-  //   alert("El ID del producto debe ser un número positivo");
-  //   setForm((prev) => ({ ...prev, id: "" }));
-  // }
+  if (form.category_id && isNaN(Number(form.category_id))) {
+    alert("El ID de categoría debe ser un número válido");
+    setForm((prev) => ({ ...prev, category_id: "" }));
+  }
+  if (form.id && isNaN(Number(form.id))) {
+    alert("El ID del producto debe ser un número válido");
+    setForm((prev) => ({ ...prev, id: "" }));
+  }
+  if (form.id && Number(form.id) < 1) {
+    alert("El ID del producto debe ser un número positivo");
+    setForm((prev) => ({ ...prev, id: "" }));
+  }
 
-  // if (form.category_id && Number(form.category_id) < 1) {
-  //   alert("El ID de categoría debe ser un número positivo");
-  //   setForm((prev) => ({ ...prev, category_id: "" }));
-  // }
-  // if (form.image_url && !form.image_url.startsWith("http")) {
-  //   alert("La URL de la imagen debe ser válida");
-  //   setForm((prev) => ({ ...prev, image_url: "" }));
-  // }
-  // if (form.image_url && form.image_url.length > 200) {
-  //   alert("La URL de la imagen no puede exceder los 200 caracteres");
-  //   setForm((prev) => ({ ...prev, image_url: "" }));
-  // }
-  // if (form.name && form.name.length < 3) {
-  //   alert("El nombre del producto debe tener al menos 3 caracteres");
-  //   setForm((prev) => ({ ...prev, name: "" }));
-  // }
-  // if (form.description && form.description.length < 10) {
-  //   alert("La descripción del producto debe tener al menos 10 caracteres");
-  //   setForm((prev) => ({ ...prev, description: "" }));
-  // }
-  // if (form.description && form.description.length > 200) {
-  //   alert("La descripción del producto no puede exceder los 200 caracteres");
-  //   setForm((prev) => ({ ...prev, description: "" }));
-  // }
-  // if (form.name && form.name.length > 50) {
-  //   alert("El nombre del producto no puede exceder los 50 caracteres");
-  //   setForm((prev) => ({ ...prev, name: "" }));
-  // }
-  //   try {
-  //     const response = await fetch(`${import.meta.env.VITE_APIURL}/products`, {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify(form),
-  //     });
-  //     if (!response.ok) {
-  //       throw new Error("Error al agregar el producto");
-  //     }
-  //     alert("Producto agregado correctamente");
-  //     setForm({
-  //       id: "",
-  //       name: "",
-  //       description: "",
-  //       price: "",
-  //       stock: "",
-  //       image_url: "",
-  //       category_id: "",
-  //     });
-  //   } catch (error) {
-  //     alert((error as Error).message);
-  //   }
-  // };
-
-  // Evento para actualizar producto
-  const handleUpdate = async () => {
-    if (!form.id) {
-      alert("Por favor ingresa el ID del producto a actualizar");
-      return;
-    }
+  if (form.category_id && Number(form.category_id) < 1) {
+    alert("El ID de categoría debe ser un número positivo");
+    setForm((prev) => ({ ...prev, category_id: "" }));
+  }
+  if (form.image_url && !form.image_url.startsWith("http")) {
+    alert("La URL de la imagen debe ser válida");
+    setForm((prev) => ({ ...prev, image_url: "" }));
+  }
+  if (form.image_url && form.image_url.length > 200) {
+    alert("La URL de la imagen no puede exceder los 200 caracteres");
+    setForm((prev) => ({ ...prev, image_url: "" }));
+  }
+  if (form.name && form.name.length < 3) {
+    alert("El nombre del producto debe tener al menos 3 caracteres");
+    setForm((prev) => ({ ...prev, name: "" }));
+  }
+  if (form.description && form.description.length < 10) {
+    alert("La descripción del producto debe tener al menos 10 caracteres");
+    setForm((prev) => ({ ...prev, description: "" }));
+  }
+  if (form.description && form.description.length > 200) {
+    alert("La descripción del producto no puede exceder los 200 caracteres");
+    setForm((prev) => ({ ...prev, description: "" }));
+  }
+  if (form.name && form.name.length > 50) {
+    alert("El nombre del producto no puede exceder los 50 caracteres");
+    setForm((prev) => ({ ...prev, name: "" }));
+  }
     try {
-      const response = await fetch(`${import.meta.env.VITE_APIURL}/products/${form.id}`, {
-        method: "PUT",
+      const response = await fetch(`${import.meta.env.VITE_APIURL}/products`, {
+        method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
       });
       if (!response.ok) {
-        throw new Error("Error al actualizar el producto");
+        throw new Error("Error al agregar el producto");
       }
-      alert("Producto actualizado correctamente");
+      alert("Producto agregado correctamente");
+      setForm({
+        id: "",
+        name: "",
+        description: "",
+        price: "",
+        stock: "",
+        image_url: "",
+        category_id: "",
+      });
     } catch (error) {
       alert((error as Error).message);
     }
   };
-
-  // Evento para borrar producto
-
 
   return (
     <>
       <div className="item-center justify-center text-black">
         <form
           className="flex flex-col gap-4 max-w-md mx-auto p-4 bg-primary rounded-3xl  shadow "
-          onSubmit={handleUpdate}
+          onSubmit={handleAdd}
         >
           <h2 className="text-center text-2xl"> Inventario</h2>
           <input
